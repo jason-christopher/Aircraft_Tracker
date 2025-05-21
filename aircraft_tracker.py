7bd2c3d68c3a6dc2ecad507fce56f44adad97379
Jason Christopher <jchristopher2448@gmail.com>
2025-05-21 15:36:57 -0500

    Switched from OpenSky to ADS-B - Major Changes

--- aircraft_tracker.py
+++ aircraft_tracker.py
@@ -3,8 +3,6 @@ import os
 import requests
 from geopy.distance import geodesic
 from twilio.rest import Client
-import time
-import re
 from dotenv import load_dotenv
 
 # --- LOAD .ENV ---
@@ -17,80 +15,116 @@ TWILIO_SID = str(os.getenv("TWILIO_SID"))
 TWILIO_AUTH_TOKEN = str(os.getenv("TWILIO_AUTH_TOKEN"))
 TWILIO_FROM = str(os.getenv("TWILIO_FROM"))
 TWILIO_TO = str(os.getenv("TWILIO_TO"))
+ADSB_KEY = str(os.getenv("ADSB_KEY"))
 
 # --- SETTINGS ---
 
-NON_MIL_CALLSIGN_PREFIXES = [
-  # Commercial and cargo airline prefixes
-  "AAL", "DAL", "UAL", "SKW", "ASA", "SWA", "JBU", "FFT", "NKS",
-  "ENY", "ASH", "RPA", "PDT", "EDV", "AWI", "QXE",
-  "FDX", "UPS", "GTI", "ABX",
-  "BAW", "AFR", "KLM", "DLH", "ACA", "QFA", "JAL", "ANA", "UAE", "THY", 
-  "CXK", "SCU", "SKU", "JIA", "FFP", "TCN", "OUA", "OST", "KOW", "UCA",
-  "LXJ",
-]
+distance_check = 50
 
 # --- FUNCTIONS ---
 
-def get_aircraft():
+def get_aircraft_open_sky():
   url = "https://opensky-network.org/api/states/all?lamin=34.0000&lomin=-99.0000&lamax=37.0000&lomax=-96.0000"
   response = requests.get(url)
   if response.status_code == 200:
     return response.json().get('states', [])
   else:
     return []
+  
+def get_aircraft_ads_b():
+  url = "https://adsbexchange-com1.p.rapidapi.com/v2/mil"
+  headers = {
+    "x-rapidapi-key": ADSB_KEY,
+    "x-rapidapi-host": "adsbexchange-com1.p.rapidapi.com"
+  }
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    aircraft_list = response.json()["ac"]
 
-def is_military(callsign):
-  # Match known commercial/cargo airline prefixes
-  for prefix in NON_MIL_CALLSIGN_PREFIXES:
-    if callsign.startswith(prefix):
-      return False
-  # Match FAA-style general aviation N-numbers
-  if re.fullmatch(r"N\d{1,5}[A-Z]{0,2}", callsign):
-    return False
-  return True
+    # Create list of filtered aircraft based on location
+    filtered_aircraft_list = []
+
+    # Iterate through each aircraft to filter by location
+    for plane in aircraft_list:
+      # Some aircraft may not have a 'lat' or 'lon' key
+      try: 
+        lat = float(plane['lat']) if plane['lat'] else None
+        lon = float(plane['lon']) if plane['lon'] else None
+        alt = int(plane['alt_baro']) if plane['alt_baro']!='ground' else 'ground'
+
+        if lat and lon and alt!='ground':
+          distance = geodesic((MY_LAT, MY_LON), (lat, lon)).miles
+          if distance <= distance_check:
+            filtered_aircraft_list.append(plane)
+      except:
+        continue
+    
+    # print(filtered_aircraft_list)
+    return filtered_aircraft_list
+  else:
+    return []
 
 def send_sms(message):
   client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
   client.messages.create(body=message, from_=TWILIO_FROM, to=TWILIO_TO)
 
 def check_nearby_aircraft():
-  aircraft_list = get_aircraft()
+  aircraft_list = get_aircraft_ads_b()
   msg = ''
-
-  # Set desired distance
-  distance_check = 50
 
   # Iterate through planes
   for plane in aircraft_list:
     try:
-      callsign = plane[1].strip().upper()
-      origin_country = plane[2]
-      lat = plane[6]
-      lon = plane[5]
-      squawk = int(plane[14]) if plane[14] else None
-      on_ground = plane[8]
-      altitude = plane[7]*3.281 if plane[7] else 'Unavailable'
+      callsign = plane['flight'].strip().upper()
+      squawk = int(plane['squawk']) if plane['squawk'] else None
+      altitude = int(plane['alt_baro']) if plane['alt_baro'] else 'Unavailable'
+      lat = float(plane['lat'])
+      lon = float(plane['lon'])
+      type = str(plane['t']) if plane['t'] else None
+      if type:
+        if type == 'TEX2':
+          type = 'T-6 Texan II'
+        elif type == 'R135':
+          type = 'RC-135 Rivet Joint'
+        elif type == 'E3TF':
+          type = 'E-3G AWACS'
+        elif type == 'H47':
+          type = 'H-47 Chinook'
+      heading_raw = float(plane['track']) if plane['track'] else None
+      if heading_raw:
+        if heading_raw <=22.5:
+          heading = 'N'
+        elif heading_raw <=67.5:
+          heading = 'NE'
+        elif heading_raw <=112.5:
+          heading = 'E'
+        elif heading_raw <=157.5:
+          heading = 'SE'
+        elif heading_raw <=202.5:
+          heading = 'S'
+        elif heading_raw <=247.5:
+          heading = 'SW'
+        elif heading_raw <=292.5:
+          heading = 'W'
+        elif heading_raw <=337.5:
+          heading = 'NW'
+        else:
+          heading = None
+      distance = geodesic((MY_LAT, MY_LON), (lat, lon)).miles
 
       # Check criteria
-      if origin_country == "United States" and lat and lon and not on_ground and callsign:
-        # Check for military aircraft
-        if is_military(callsign):
-          distance = geodesic((MY_LAT, MY_LON), (lat, lon)).miles
-          if distance <= distance_check:
-            msg += f"Military aircraft '{callsign}' detected {distance:.2f} miles away at {altitude:.0f} feet.\n"
+      if callsign and callsign != '0':
+        msg += f"{callsign} ({type}) detected {distance:.1f} miles away at {altitude:,} ft{', heading ' + heading if heading else ''}.\n"
         
         # Check for emergency
         if squawk in [7500, 7600, 7700]:
-          distance = geodesic((MY_LAT, MY_LON), (lat, lon)).miles
-          if distance <= distance_check:
-            msg += f"'{callsign}' squawking {squawk} {distance:.2f} miles away at {altitude:.0f} feet.\n"
+          msg += f"'{callsign}' squawking {squawk} {distance:.2f} miles away at {altitude:,} ft.\n"
 
     except Exception as e:
-      print(f"Error processing aircraft: {e}")
+      print(f"Error processing {callsign}: {e}")
   print(msg)
-  # if msg:
-    # send_sms(msg)
+  # # if msg:
+  #   # send_sms(msg)
 
 # --- MAIN LOOP ---
 
